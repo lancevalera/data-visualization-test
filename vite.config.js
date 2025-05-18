a890e7ea4ba01faf0b4dc68e7bfdd0d0ff69d536
Lance V <lancev@macbookpro.home>
2025-05-18 14:53:55 -0400

    Add absolute imports

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      hooks: "/src/hooks"
+    }
+  },
   server: {
     port: 8080,
     open: true
